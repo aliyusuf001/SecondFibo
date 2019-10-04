bc3a86cc5ac58a5b80bf0d9017680e8b56a97fb7
Yusuf Ali <yusufalioladimeji2000@gmail.com>
2019-10-04 16:11:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondFibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong count, firstNumber = 1, secondNumber = 1, nestNumber;
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            for (count = 2; count < 698; count++)
+            {
+                nestNumber = firstNumber + secondNumber;
+                Console.WriteLine(nestNumber);
+
+                firstNumber = secondNumber;
+                secondNumber = nestNumber;
+            }
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
